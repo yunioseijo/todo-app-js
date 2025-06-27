52ed4f5b6ba0d5227a7a2f39e465835f173a55b1
yunioseijo <yunioseijo@gmail.com>
2025-06-27 19:50:47 +0200

    refactor: enhance App component with input listeners for adding and toggling todos

--- src/todos/app.js
+++ src/todos/app.js
@@ -4,6 +4,7 @@ import { renderTodos } from "./use-cases/render-todos";
 
 const ElementIds = {
   TodoList: ".todo-list",
+  NewTodoInput: "#new-todo-input",
 };
 
 /** @param {string} elementId */
@@ -12,11 +13,29 @@ export const App = (elementId) => {
     const todos = todoStore.getTodos(todoStore.getCurrentFilter());
     renderTodos(ElementIds.TodoList, todos);
   };
-
+  // Display the initial list of todos when App() is called
   (() => {
     const app = document.createElement("div");
     app.innerHTML = `${html}`;
     document.querySelector(elementId).append(app);
     displayTodos();
   })();
+  //Reference HTML
+  const newDescriptionInput = document.querySelector(ElementIds.NewTodoInput);
+  const todoListUl = document.querySelector(ElementIds.TodoList);
+
+  //Inputs Listeners
+  newDescriptionInput.addEventListener("keyup", (event) => {
+    if (event.key !== "Enter") return;
+    if (!newDescriptionInput.value || !newDescriptionInput.value.trim()) return;
+    todoStore.addTodo(newDescriptionInput.value);
+    displayTodos();
+    newDescriptionInput.value = "";
+  });
+  todoListUl.addEventListener("click", (event) => {
+    const element = event.target.closest("[data-id]");
+    if (!element) return;
+    todoStore.toggleTodo(element.getAttribute("data-id"));
+    displayTodos();
+  });
 };
