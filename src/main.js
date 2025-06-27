27a496ecf90dc7216edd7d65c4a3f2eae053886b
yunioseijo <yunioseijo@gmail.com>
2025-06-27 12:36:49 +0200

    structured created

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import "./style.css";
+import { App } from "./todos/app.js";
+console.log("Hello from main.js");
+App("#app");
---
+++ src/todos/app.js
@@ -0,0 +1,9 @@
+import html from "./app.html?raw";
+/** @param {string} elementId */
+export const App = (elementId) => {
+  (() => {
+    const app = document.createElement("div");
+    app.innerHTML = `${html}`;
+    document.querySelector(elementId).append(app);
+  })();
+};
