5537d0d493ce6b74a0cb4a120768096bd33a2eaa
yunioseijo <yunioseijo@gmail.com>
2025-06-27 14:17:36 +0200

    add uuid dependency and update Todo model to use UUID for unique identifiers

--- src/todos/models/todo.js
+++ src/todos/models/todo.js
@@ -1,3 +1,4 @@
+import { v4 as uuid } from "uuid";
 export class Todo {
   /**
    * Initializes a new instance of the Todo class.
@@ -5,7 +6,7 @@ export class Todo {
    * Sets the initial state of the todo as not done and assigns the current date to createdAt.
    */
   constructor(description) {
-    this.id = 1;
+    this.id = uuid();
     this.description = description;
     this.done = false;
     this.createdAt = new Date();
